6194e4c49c8e8df7a6e8ed569d2e0e32da14d81f
Evt-Nadya <49037722+Evt-Nadya@users.noreply.github.com>
2019-03-31 21:13:08 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,6 +1,3 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include "pch.h"
 #include <iostream>
 #include <string>
