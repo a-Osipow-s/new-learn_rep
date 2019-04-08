e2c05dd7dc032db32f4f372d65ce9c777afc7e2c
v-piatakova <49119241+v-piatakova@users.noreply.github.com>
2019-04-08 21:52:38 +0300

    Rename 345678.cpp to backets.cpp

---
+++ backets.cpp
@@ -0,0 +1,28 @@
+﻿// 345678.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	char line[20] =" (()()){}{{}}[()]" ;
+	
+	int basket = 0;
+	for (int i = 0; i < 20; i++)
+	{
+		if (line[i] == ')') basket++;
+		if (line[i] == '(') basket--;
+		if (line[i] == ']') basket++;
+		if (line[i] == '[') basket--;
+		if (line[i] == '}') basket++;
+		if (line[i] == '{') basket--;
+	}
+	if (basket > 0) cout << "net otkrivaushey skobki" << endl;
+	if (basket < 0) cout << "net zakrivaushey skobki" << endl;
+	if (basket == 0) cout << "pravilnaya rastanovka" << endl;
+
+	return 0;
+}
