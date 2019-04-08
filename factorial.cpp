13fc5947fcac709d7934a878bf04806d7ad5b3bc
Илья Лежепеков <77-0175@mail.ru>
2019-04-08 19:34:46 +0300

    factorial

---
+++ factorial.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+long double faktor(int n)
+{
+    if(n<0)
+        return 0;
+    if(n==0)
+        return 1;
+    else
+        return n*faktor(n-1);
+}
+
+int main()
+{
+    int n;
+    cout<<"input faktor"<<endl;
+    cin>>n;
+    faktor(n);
+    cout<<faktor(n)<<endl;
+    return 228;
+    
+}
