78990e6e1485b38e9a68529eb60f28f1f3593874
a-Osipow-s <toni.osipow@mail.ru>
2019-04-11 23:18:59 +0300

    test

---
+++ test.cpp
@@ -0,0 +1,14 @@
+// ConsoleApplication8.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world" << endl;
+	return 0;
+}
+
